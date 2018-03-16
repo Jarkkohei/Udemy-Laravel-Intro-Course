02a4d56ebb788de092688d5b3097d6ff81ac1bf7
Jarkko Heikkinen <jarkko.heikkinen@mediasignal.fi>
2018-03-16 13:46:44 +0200

    Added the authentication. #45

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -6,7 +6,22 @@
         text-align: left;
     }
 </style>
-<form action="" method="POST">
+
+@if(count($errors) > 0)
+    <section class="info-box fail">
+        @foreach($errors->all() as $error)
+            {{ $error }}
+        @endforeach
+    </section>
+@endif
+
+@if(Session::has('fail'))
+    <section class="info-box fail">
+       {{ Sessio::get('fail') }}
+    </section>
+@endif
+
+<form action="{{ route('admin.login') }}" method="POST">
     <div class="input-group">
         <label for="name">Your Name:</label>
         <input type="text" name="name" id="name" placeholder="Your name">
@@ -15,5 +30,6 @@
         <input type="password" name="password" id="password" placeholder="Your password">
     </div>
     <button type="submit">Login</button>
+<input type="hidden" name="_token" value="{{ Session::token() }}">
 </form>
 @endsection
