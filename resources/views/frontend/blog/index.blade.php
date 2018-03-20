891dfd6cb6950b7af87daf56b26e9caf6bca1b87
Jarkko Heikkinen <jarkko.heikkinen@mediasignal.fi>
2018-03-20 11:24:07 +0200

    Added the listing views for Posts. Both frontend and backend. #57

--- resources/views/frontend/blog/index.blade.php
+++ resources/views/frontend/blog/index.blade.php
@@ -4,16 +4,31 @@
     Blog index
 @endsection
 
+@section('styles')
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/font-awesome.min.css">
+@endsection
+
 @section('content')
-    <article class="blog-post">
-        <h3>Post Title</h3>
-        <span class="subtitle">Post Author | Date</span>
-        <p>Post Body</p>
-        <a href="#">Read more</a>
-    </article>
+    @foreach($posts as $post)
+        <article class="blog-post">
+            <h3>{{ $post->title }}</h3>
+            <span class="subtitle">{{ $post->author }} | {{ $post->created_at }}</span>
+            <p>{{ $post->body }}</p>
+            <a href="#">Read more</a>
+        </article>
+    @endforeach
 
-    <section class="pagination">
-        Pagination
-    </section>
-
+    <!-- If more than one page of results -->
+    @if($posts->lastPage() > 1)
+        <section class="pagination">
+            <!-- If not on first page -->
+            @if($posts->currentPage() != 1)
+                <a href="{{ $posts->previousPageUrl() }}"><i class="fa fa-caret-left"></i></a>
+            @endif
+            <!-- If not on last page -->
+            @if($posts->currentPage() != $posts-lastPage())
+                <a href="{{ $posts->nextPageUrl() }}"><i class="fa fa-caret-right"></i></a>
+            @endif
+        </section>
+    @endif
 @endsection
